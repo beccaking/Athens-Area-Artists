4a5d695caf4356d8f36b791d2b69c96b3338f737
Becca King <becca.king3232@gmail.com>
2019-11-06 17:43:20 -0500

    Created edit and create routes

--- controllers/tattoos.js
+++ controllers/tattoos.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const tattoos = require('../models/tattoos.js')
 const Collection = require('../models/collection.js')
 
+//Routes for a logged-in user
 
-//Collection page with save buttons, Logged in only
+//Index page with save buttons
 router.get('/', (req, res) => {
   if(req.session.username){
     res.render('tattoos/index.ejs', {
@@ -16,15 +17,7 @@ router.get('/', (req, res) => {
     res.redirect('/')
   }
 })
-
-//Save a tattoo from the index page to your collection, Logged in only
-router.post('/my-collection/:id', (req, res) => {
-    Collection.create(tattoos[req.params.id], {user: req.session.username}, (error, createdUser) => {
-      res.redirect('/tattoos')
-    })
-})
-
-//Personal collection page, Logged in only
+//Personal collection page
 router.get('/my-collection', (req, res) => {
     Collection.find({user:req.session.username}, (error, allTattoos) => {
       res.render('tattoos/collection.ejs', {
@@ -32,13 +25,48 @@ router.get('/my-collection', (req, res) => {
       })
     })
 })
-
-//Tattoo show page with save button
+//Index show page with save button
 router.get('/:id', (req, res) => {
   res.render('tattoos/show.ejs', {
     tattoo: tattoos[req.params.id],
     index: req.params.id
   })
+})
+//Create page
+router.get('/my-collection/create', (req, res) => {
+  res.render('tattoos/create.ejs')
+})
+//Edit page
+router.get('/my-collection/:id/edit', (req, res) => {
+  Collection.findById(req.params.id, (error, foundObject) => {
+    res.render('tattoos/edit.ejs', {
+      tattoo: foundObject
+    })
+  })
+})
+//Personal collection show page
+router.get('/my-collection/:id', (req, res) => {
+  Collection.find({user:req.session.username}, (error, allTattoos) => {
+    res.render('tattoos/collectionshow.ejs', {
+      tattoo: allTattoos[req.params.id]
+    })
+  })
+})
+//Create route, brand new
+router.post('/my-collection', (req, res) => {
+  Collection.create(req.body, (error, createdElement) => {
+    Collection.findByIdAndUpdate(createdElement.id, {$set:{user: req.session.username}}, (error, updatedElement) => {
+      res.redirect('/tattoos/my-collection')
+      })
+    })
+  })
+//Create route, save from index page
+router.post('/my-collection/:id', (req, res) => {
+    Collection.create(tattoos[req.params.id], (error, createdElement) => {
+      Collection.findByIdAndUpdate(createdElement.id, {$set: {user: req.session.username}}, (error, updatedElement) => {
+          res.redirect('/tattoos')
+      })
+    })
 })
 
 //Log Out
@@ -48,5 +76,25 @@ router.delete('/', (req, res) => {
   flag = false
 })
 
+//Delete an item from collection
+router.delete('/my-collection/:id', (req, res) => {
+  Collection.findByIdAndRemove(req.params.id, (error, deletedObject) => {
+    res.redirect('/tattoos/my-collection')
+  })
+})
+
+
+//Update route
+router.put('/my-collection/:id/edit', (req, res) => {
+  Collection.findByIdAndUpdate(req.params.id, {$set: {artist: req.body.artist}}, (error, UpdatedElement) => {
+    Collection.findByIdAndUpdate(req.params.id, {$set: {studio: req.body.studio}}, (error, UpdatedElement) => {
+      Collection.findByIdAndUpdate(req.params.id, {$set: {date: req.body.date}}, (error, UpdatedElement) => {
+        Collection.findByIdAndUpdate(req.params.id, {$set: {img: req.body.img}}, (error, UpdatedElement) => {
+            res.redirect('/tattoos/my-collection')
+        })
+      })
+    })
+  })
+})
 
 module.exports = router
