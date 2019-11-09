ccc85f83a61eee17b5c249721965da2e2f8f4edf
Becca King <becca.king3232@gmail.com>
2019-11-09 15:58:18 -0500

    Changed dropdown to click to show for mobile

--- public/jquery.js
+++ public/jquery.js
@@ -1,14 +1,14 @@
 $(() => {
 
-if($('.dropdown').hasAttribute('class', 'showing')){
-    $('.dropdown').on('click', () => {
+let boxShowing = false
+
+$('.dropdown').on('click', () => {
+    if(boxShowing === true){
       $('.dropdown-content').css('display', 'none')
-      $('.dropdown').removeAttribute('class', 'showing')
-    })
-  } else {
-    $('.dropdown').on('click', () => {
+      boxShowing = false
+    } else {
       $('.dropdown-content').css('display', 'block')
-      $('.dropdown').setAttribute('class', 'showing')
-    })
-  }
+      boxShowing = true
+    }
+  })
 })
